c0e34eedc2cc39c0a1e01b312c6910db7fa30a68
Ethan Wilansky <ewilansky@yahoo.com>
2018-12-13 08:26:34 -0500

    added prizm image & container

---
+++ prizm/ubuntu16.04/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker image build . --tag "prizm:ubuntu16.04" --no-cache
---
+++ prizm/ubuntu16.04/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# docker run -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 --rm -it --name prizm prizmdocker-ubuntu16.04:latest
+# docker run -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 -it --name prizm prizm:ubuntu16.04
+docker run -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 -it --name prizm prizmdocker-ubuntu16.04:latest
